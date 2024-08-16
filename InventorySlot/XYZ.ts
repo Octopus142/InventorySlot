ce1eb13bef93ff8d99fed9613637c2054a900a2d
Opus <blackfairyforma726@gmail.com>
2024-08-16 21:21:47 +0800

    Update XYZ.ts

--- InventorySlot/XYZ.ts
+++ InventorySlot/XYZ.ts
@@ -1,3 +1,7 @@
+import 'RETURN.ts'
 declare module X{}
 declare module Y{}
 declare module Z{}
+export const X = 'a + b + c + d + e + f + g + h'
+export const Y = 'i + j + k + l + m + n + o + p'
+export const Z = 'q + r + s + t + u + v + w'
