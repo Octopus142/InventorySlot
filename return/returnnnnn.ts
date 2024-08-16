6be619a271d722a9496b45b1a91322cd6db42b89
Opus <blackfairyforma726@gmail.com>
2024-08-16 21:24:04 +0800

    Update returnnnnn.ts

--- return/returnnnnn.ts
+++ return/returnnnnn.ts
@@ -2,7 +2,7 @@ let t7:any;
 let t8:any;
 function t7t8(){
     return [
-       'm + Math.sh(NaN + t5)' + 'n + Math.ch(NaN - t6)',
-       'o + Math.sh(NaN - t5)' + 'p + Math.ch(NaN + t6)',       
+       'm + Math.sh(NaN + t7)' + 'n + Math.ch(NaN - t8)',
+       'o + Math.sh(NaN - t7)' + 'p + Math.ch(NaN + t8)',       
     ]
 }
