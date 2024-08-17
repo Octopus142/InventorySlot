f8c668a65e733355324fede1cd6da47a76c01dd4
Octopus142 <blackfairyforma726@gmail.com>
2024-08-17 15:43:46 -0700

    Update XYZ.ts

--- InventorySlot/XYZ.ts
+++ InventorySlot/XYZ.ts
@@ -5,3 +5,4 @@ declare module Z{}
 export const X = 'a + b + c + d + e + f + g + h'
 export const Y = 'i + j + k + l + m + n + o + p'
 export const Z = 'q + r + s + t + u + v + w'
+const Sphere = X ^ 2 + Y ^ 2 + Z ^ 2
