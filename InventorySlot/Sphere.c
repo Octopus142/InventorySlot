a3ac50cdddb60317d6676a8dde56e63c47c68a76
Octopus142 <blackfairyforma726@gmail.com>
2024-09-12 13:19:07 +0800

    Update Sphere.c

--- InventorySlot/Sphere.c
+++ InventorySlot/Sphere.c
@@ -1,14 +1,14 @@
 #include "InventorySlot84.ts"
 #include "InventorySlot369.ts"
 #include "InventorySlot1234.ts"
-#include "InventorySlot567890.ts"
+#include "InventorySlot567.ts"
 #include "N0InventorySlot.ts"
 #include "N1InventorySlot.ts"
 #include "XYZ.ts"
 #include "alphabet.c"
 void InventortSlot(){
     int InventorySlot2017 = InventorySlot84 + InventorySlot369;
-    int InventorySlot520 = InventorySlot1234 + InventorySlot567890;
+    int InventorySlot520 = InventorySlot1234 + InventorySlot567;
     int NInventorySlot = N1InventorySlot + N0InventorySlot;
 }
 void Sphere(){
